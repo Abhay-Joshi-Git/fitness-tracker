c06a5db13acc3b010fc75e9950a821ee8880665c
abjoshi <abjoshi@voceracommunications.com>
2019-07-22 11:17:19 +0530

    table responsive style changes

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 import { Subscription } from 'rxjs';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-past-training',
@@ -10,6 +11,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./past-training.component.scss']
 })
 export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
+  isMobile = false;
   private doneExercisesSubscription: Subscription = Subscription.EMPTY;
   columnsToDisplay = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
@@ -17,17 +19,22 @@ export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private readonly _trainingExercise: TrainingService) { }
+  constructor(private readonly _trainingExercise: TrainingService, private readonly breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
     this.doneExercisesSubscription = this._trainingExercise.doneExercises.subscribe(exercises => {
       this.dataSource.data = exercises;
     });
+
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.breakpointObserver.observe(['(max-width: 599px)']).subscribe(result => {
+      this.isMobile = result.matches;
+      this.dataSource.paginator = result.matches ? null : this.paginator;
+    });
   }
 
   applyFilter(filterValue: string) {
